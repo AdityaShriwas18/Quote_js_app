2446d8aa6a7b2fe917790c6dff5cbef0b5cbbc30
AdityaShriwas18 <147526826+AdityaShriwas18@users.noreply.github.com>
2024-06-30 07:39:16 -0700

    delete file

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
 let card = document.querySelector('.quote-list')
-let savedcard = document.querySelector('.saved-quote-list')
+let savedCard = document.querySelector('.saved-quote-list')
 
 
 async function getRandomQuote(){
     
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 50; i++) {
         try{
             const response = await fetch('https://api.freeapi.app/api/v1/public/quotes/quote/random')
             const data = await response.json()
-            let Author = data.data.author;
-            let Quote = data.data.content;
-            console.log(data.data.content);
-            console.log(data.data.author);
-        
+            var Author = data.data.author;
+            var Quote = data.data.content;
+
+            // let savebtn = document.createElement('button')
+            // savebtn.classList.add('savebtn')
+
             let content  = document.createElement('li')
             content.innerHTML = `<div class="card">
                 <p class="quote-content">" ${Quote} "</p>
-                <button class="savebtn" >Save</button>
                 <h3 class="content-author" >~${Author}</h3>
-              </div>`
-        
+                <button class="savebtn" >Save</button>
+                </div>`
+                
             card.appendChild(content)
     
         } catch(error){
@@ -35,7 +36,6 @@ getRandomQuote()
 
 const hamburger = document.getElementById('hamburger');
         const navbar = document.getElementById('navbar');
-
         hamburger.addEventListener('click', () => {
             navbar.classList.toggle('open');
             hamburger.classList.toggle('open');
@@ -45,7 +45,6 @@ const hamburger = document.getElementById('hamburger');
 
 const savebtn = document.querySelector('.savebtn')
 
-
 function saveCardToLocalStorage(Quote, Author) {
     const cards = JSON.parse(localStorage.getItem('savedCards')) || [];
 
@@ -54,16 +53,15 @@ function saveCardToLocalStorage(Quote, Author) {
     localStorage.setItem('savedCards', JSON.stringify(cards));
 }
 
-
 function loadSavedCards() {
     const cards = JSON.parse(localStorage.getItem('savedCards')) || [];
     cards.forEach(card => {
         const li = document.createElement('li');
         li.innerHTML = `
             <div class="card">
-                <p class="quote-content">"${card.quote}"</p>
+                <p class="quote-content">"${card.Quote}"</p>
                 <button class="deletebtn">Delete</button>
-                <h3 class="content-author">~${card.author}</h3>
+                <h3 class="content-author">~${card.Author}</h3>
             </div>`;
         savedCards.appendChild(li);
     });
@@ -78,6 +76,7 @@ savebtn.addEventListener('click',()=>{
                 <button class="deletebtn">Delete</button>
                 <h3 class="content-author">~${Author}</h3>
             </div>`;
+        console.log("save clicked");
         saveCardToLocalStorage(Quote, Author);
         savedCards.appendChild(li);
 
--- tempCodeRunnerFile.js
+++
@@ -1,4 +0,0 @@
-fetch('https://api.freeapi.app/api/v1/public/quotes/quote/random')
-// .then((response)=>{
-//     return response;
-// }).catch((err)=> console.log(err))
